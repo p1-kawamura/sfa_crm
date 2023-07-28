54bb6cf87c700bc792db417b9e0de78919f3aca6
kawamura <lailai666@hotmail.com>
2023-07-29 01:33:18 +0900

    desk0133

--- sfa_crm/sfa_crm/settings.py
+++ sfa_crm/sfa_crm/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = 'static/'
 
 NUMBER_GROUPING = 3
 SESSION_SAVE_EVERY_REQUEST = True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
