ac1fffbad913086adc606ee1ae2bf50de6896816
p1-kawamura <kawamura@p1-intl.com>
2023-10-25 13:18:28 +0900

    com1318

--- sfa_crm/sfa_crm/settings.py
+++ sfa_crm/sfa_crm/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%5-y79s$e6o14v%y=jx=uddfcz-hlijb2emc17hw^sr1c&m3y1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
