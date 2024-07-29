b0388a1d8f6c6d4f2bcaabb0a69ce8d0c26bf062
p1-kawamura <kawamura@p1-intl.com>
2024-07-29 13:06:53 +0900

    com1306

--- sfa_crm/sfa/views.py
+++ sfa_crm/sfa/views.py
@@ -1403,7 +1403,7 @@ def kanri_index(request):
         request.session["search"]={}
     if "tantou" not in request.session["search"]:
         request.session["search"]["tantou"]=""
-    ins=Credit_url.objects.all().order_by("day").reverse()[:50]
+    ins=Credit_url.objects.all().order_by("day").reverse()[:300]
     # アクティブ担当
     act_id=request.session["search"]["tantou"]
     if act_id=="":
